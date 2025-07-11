c7530061bf55d72f611bfab9e2c554b584d55a99
Anishjain07 <131429165+Anishjain07@users.noreply.github.com>
2025-07-11 09:47:53 +0000

    conversion of datatypes

---
+++ 01_bas/conversion.js
@@ -0,0 +1,14 @@
+let score ="33"
+//null ka value 0 true ka 1 "33"=>33
+//"33abc"=> NaN
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber= Number(score)
+//console.log(typeof valueInNumber);
+ //" "=> false 
+
+let someNumber =23
+let stringNumber =String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
