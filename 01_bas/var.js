b4e89f4fbd9fbedc18ee232539a17ce30461b86d
Anishjain07 <131429165+Anishjain07@users.noreply.github.com>
2025-07-11 08:38:03 +0000

    done variable

---
+++ 01_bas/var.js
@@ -0,0 +1,9 @@
+const accountId = 16752
+let accountEmail = "aj@gmail.com"
+var accoundpassword ="12345"
+accountCity= "jaipur"
+
+//accountId= 2 not allowed
+accountEmail = "hhh@jjs.com"
+console.log(accountId);
+console.table([accountId, accountEmail,accoundpassword, accountCity])
