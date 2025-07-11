595469c43aa8d1e1772e07548ed3208e75ae9ee3
Anishjain07 <131429165+Anishjain07@users.noreply.github.com>
2025-07-11 08:52:09 +0000

    datatypes

---
+++ 01_bas/datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; //treats all jscode as new version
+
+let name ="anish" //string
+let age =18 // number
+let isLoggedIn= false //bool
+let state;
+//number =>2 to pwer 53
+//bigint
+//string=> ""
+//boolean 
+//null=> standalone value
+// undefined => 
+//symbol=> unique
+
+//object
+
+console.log(typeof age);//type of null is objects
