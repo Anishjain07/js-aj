8df6f1f623437b274464bac0036940a014cf7ffc
Anishjain07 <131429165+Anishjain07@users.noreply.github.com>
2025-07-11 15:27:35 +0000

    new

---
+++ 01_bas/comparison.js
@@ -0,0 +1,17 @@
+//console.log(2>1);
+//console.log(2>=1)
+//console.log(2<1);
+//console.log(2==1);///True or false aata
+
+console.log("2" >1);
+console.log("02" >1);
+//data types same rakhna imp hai
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+console.log(undefined ==0);
+console.log(undefined >0);
+console.log(undefined <0);
+/// ===
+
+console.log("2" ===2)
--- 01_bas/conversion.js
+++ 01_bas/conversion.js
@@ -8,7 +8,31 @@ let score ="33"
 //console.log(typeof valueInNumber);
  //" "=> false 
 
-let someNumber =23
-let stringNumber =String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let someNumber =23
+//let stringNumber =String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// Operations
+//let value =3
+//let negValue =-value
+//console.log(negValue);
+//add sub multi  etc.,
+
+let str1= "hello"
+let str2 =" anish"
+
+let str3 =str1+ str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+//console.log(+true);//true ka true hi aata +trrue ka 1 true+ ka error
+//console.log(+"");//value 0
+
+let gameCou =100
+gameCou++;
+console.log(gameCou);
